ab74c73243283eb3eaa328d707873475e6668af7
clynamen <clynamen@gmail.com>
2020-12-15 21:06:44 +0100

    add 12b

--- 12/main.hs
+++ 12/main.hs
@@ -8,9 +8,23 @@ type Yaw = Int
 addVec2 :: Vec2 -> Vec2 -> Vec2
 addVec2 (ax, ay) (bx, by) = (ax+bx, ay+by)
 
-type ShipState = (Vec2, Yaw)
+subVec2 :: Vec2 -> Vec2 -> Vec2
+subVec2 (ax, ay) (bx, by) = (ax-bx, ay-by)
 
-addYaw :: Yaw -> Yaw -> Yaw 
+
+rotateVec2 :: Vec2 -> Int -> Vec2
+rotateVec2 (ax, ay) degrees =
+    let fx = fromIntegral ax
+        fy = fromIntegral ay
+        rad = fromIntegral degrees / 180.0  * pi :: Float
+        bx = fx * cos rad  - fy * sin  rad
+        by = fx * sin rad  + fy * cos  rad
+    in (round bx, round by)
+
+type WaypointState = (Vec2, Yaw)
+type ShipState = (Vec2, WaypointState)
+
+addYaw :: Yaw -> Yaw -> Yaw
 addYaw a b = (a + b) `mod` 360
 
 getDirectionFromYaw :: Yaw -> Char
@@ -19,7 +33,7 @@ getDirectionFromYaw 90 = 'N'
 getDirectionFromYaw 180 = 'W'
 getDirectionFromYaw 270 = 'S'
 
-applyAction :: ShipState -> Action -> ShipState
+applyAction :: WaypointState -> Action -> WaypointState
 applyAction (vec2, yaw) ('N', value) = (vec2 `addVec2` (0, value), yaw)
 applyAction (vec2, yaw) ('S', value) = (vec2 `addVec2` (0, -value), yaw)
 applyAction (vec2, yaw) ('E', value) = (vec2 `addVec2` (value, 0), yaw)
@@ -27,6 +41,20 @@ applyAction (vec2, yaw) ('W', value) = (vec2 `addVec2` (-value, 0), yaw)
 applyAction (vec2, yaw) ('L', value) = (vec2, yaw `addYaw` value)
 applyAction (vec2, yaw) ('R', value) = (vec2, yaw `addYaw` (-value) )
 applyAction (vec2, yaw) ('F', value) = applyAction (vec2, yaw) (getDirectionFromYaw yaw, value)
+
+
+rotateWaypoint :: WaypointState -> Int -> WaypointState
+rotateWaypoint (vec2, yaw) degrees =
+    (rotateVec2 vec2 degrees, yaw)
+
+imulVec2 :: Vec2 -> Int -> Vec2
+imulVec2 (ax, bx) m = (ax * m, bx * m)
+
+applyAction2 :: ShipState -> Action -> ShipState
+applyAction2 (vec2, waypointState) ('L', value) = (vec2, rotateWaypoint waypointState value    )
+applyAction2 (vec2, waypointState) ('R', value) = (vec2, rotateWaypoint waypointState (-value) )
+applyAction2 (vec2, waypointState@(waypointVec2, _)) ('F', value) = (vec2 `addVec2` (imulVec2 waypointVec2 value), waypointState) 
+applyAction2 (shipVec2, waypointState) action = (shipVec2, applyAction waypointState action)
 
 
 lineToAction :: String -> Action
@@ -39,20 +67,31 @@ getActionsFromFile fname = do
     let actionLines = lines content
     return $ map lineToAction actionLines
 
-start :: ShipState
+start :: WaypointState
 start = ((0, 0), 0)
 
-applyActions :: Actions -> ShipState -> ShipState
+startShipState :: ShipState
+startShipState = ((0, 0), ((10, 1), 0))
+
+applyActions :: Actions -> WaypointState -> WaypointState
 applyActions actions state =
     foldl applyAction state actions
 
-manhDist :: ShipState -> ShipState -> Int
-manhDist ((ax, ay), _)  ((bx, by), _) = abs (ax-bx) + abs (ay-by)
+applyActions2 :: Actions -> ShipState -> ShipState
+applyActions2 actions state =
+    foldl applyAction2 state actions
+
+manhDist :: Vec2 -> Vec2 -> Int
+manhDist (ax, ay) (bx, by) = abs (ax-bx) + abs (ay-by)
+
 
 main :: IO ()
 main = do
     actions <- getActionsFromFile "input_12a.txt"
     let end1 = applyActions actions start
     print $ end1
-    print $ manhDist end1 start
+    print $ manhDist (fst end1) (fst start)
+    let end2 = applyActions2 actions startShipState
+    print $ end2
+    print $ manhDist (fst end2) (fst startShipState)
 
